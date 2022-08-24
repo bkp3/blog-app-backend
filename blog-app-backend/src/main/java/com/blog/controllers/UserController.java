1bfd21a3506474ccb728b540a58d1dacbafc37db
bkp3 <bholakumar.7372@gmail.com>
2022-08-24 23:29:26 +0530

    Validating Data using Bean Validator

--- blog-app-backend/src/main/java/com/blog/controllers/UserController.java
+++ blog-app-backend/src/main/java/com/blog/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.blog.controllers;
 import java.util.List;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,13 +30,13 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
 		UserDto createdUserDto = this.userService.createUser(userDto);
 		return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/{id}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("id") int userId) {
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("id") int userId) {
 		UserDto updateUserDto = this.userService.updateUser(userDto, userId);
 		return ResponseEntity.ok(updateUserDto);
 	}
@@ -54,6 +56,5 @@ public class UserController {
 	public ResponseEntity<UserDto> getSingleUser(@PathVariable int userId) {
 		return ResponseEntity.ok(this.userService.getUserById(userId));
 	}
-	
 
 }
--- blog-app-backend/src/main/java/com/blog/payloads/UserDto.java
+++ blog-app-backend/src/main/java/com/blog/payloads/UserDto.java
@@ -1,5 +1,10 @@
 package com.blog.payloads;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +15,19 @@ import lombok.Setter;
 public class UserDto {
 
 	private int id;
+
+	@NotEmpty
+	@Size(min = 4, message = "username must be min of 4 characters")
 	private String name;
+
+	@Email(message = "Email address is not valid!")
 	private String email;
+
+	@NotEmpty
+	@Size(min = 3, max = 10, message = "Passord must be min of 3 chars and max of 10 chars")
 	private String password;
+
+	@NotEmpty
 	private String about;
 
 }
