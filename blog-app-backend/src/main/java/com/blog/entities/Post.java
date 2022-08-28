265804e7c9a28d077cec86a127a9955c4c86d2dd
bkp3 <bholakumar.7372@gmail.com>
2022-08-28 23:57:36 +0530

    Post entity

---
+++ blog-app-backend/src/main/java/com/blog/entities/Post.java
@@ -0,0 +1,40 @@
+package com.blog.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Post {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int postId;
+
+	@Column(name = "post_title", length = 100, nullable = false)
+	private String title;
+
+	@Column(length = 10000)
+	private String content;
+	private String imageName;
+	private Date addedDate;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne
+	private User user;
+
+}
