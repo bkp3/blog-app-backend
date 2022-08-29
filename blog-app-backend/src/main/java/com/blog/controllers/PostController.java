9be7eb0b119f2fd7d0b11b8731210899df4dd8be
bkp3 <bholakumar.7372@gmail.com>
2022-08-29 22:43:00 +0530

    POST API

--- blog-app-backend/src/main/java/com/blog/controllers/PostController.java
+++ blog-app-backend/src/main/java/com/blog/controllers/PostController.java
@@ -1,8 +1,11 @@
 package com.blog.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +30,22 @@ public class PostController {
 		PostDto createPost = this.postService.createPost(postDto, userId, categoryId);
 		return new ResponseEntity<PostDto>(createPost, HttpStatus.OK);
 
+	}
+
+	// get by user
+
+	@GetMapping("/user/{userId}/posts")
+	public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable int userId) {
+		List<PostDto> posts = this.postService.getPostsByUser(userId);
+		return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
+	}
+
+	// get by category
+
+	@GetMapping("/category/{categoryId}/posts")
+	public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable int categoryId) {
+		List<PostDto> posts = this.postService.getPostsByCategory(categoryId);
+		return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);
 	}
 
 }
--- blog-app-backend/src/main/java/com/blog/services/impl/PostServiceImpl.java
+++ blog-app-backend/src/main/java/com/blog/services/impl/PostServiceImpl.java
@@ -2,6 +2,7 @@ package com.blog.services.impl;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -77,14 +78,30 @@ public class PostServiceImpl implements PostService {
 
 	@Override
 	public List<PostDto> getPostsByCategory(int categoryId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Category category = this.categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResourceNotFoundException("Category ", "Category ID", categoryId));
+
+		List<Post> posts = this.postRepository.findByCategory(category);
+
+		List<PostDto> postsDto = posts.stream().map((post) -> this.modelMapper.map(post, PostDto.class))
+				.collect(Collectors.toList());
+
+		return postsDto;
 	}
 
 	@Override
 	public List<PostDto> getPostsByUser(int userId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		User user = this.userRepository.findById(userId)
+				.orElseThrow(() -> new ResourceNotFoundException("User ", "user ID", userId));
+
+		List<Post> posts = this.postRepository.findByUser(user);
+
+		List<PostDto> postDtos = posts.stream().map((post) -> this.modelMapper.map(post, PostDto.class))
+				.collect(Collectors.toList());
+
+		return postDtos;
 	}
 
 	@Override
