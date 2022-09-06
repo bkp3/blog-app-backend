7d30896241a78668a49a3b6e1a7433f7f84dc7dc
bkp3 <bholakumar.7372@gmail.com>
2022-09-06 22:31:01 +0530

    Basic Authentication with Database

--- blog-app-backend/src/main/java/com/blog/BlogAppBackendApplication.java
+++ blog-app-backend/src/main/java/com/blog/BlogAppBackendApplication.java
@@ -1,12 +1,18 @@
 package com.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class BlogAppBackendApplication {
+public class BlogAppBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppBackendApplication.class, args);
@@ -15,6 +21,13 @@ public class BlogAppBackendApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(this.passwordEncoder.encode("1234"));
+
 	}
 
 }
---
+++ blog-app-backend/src/main/java/com/blog/security/CustomUserDetailService.java
@@ -0,0 +1,28 @@
+package com.blog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.blog.entities.User;
+import com.blog.exceptions.ResourceNotFoundException;
+import com.blog.repositories.UserRepository;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User user = this.userRepository.findByEmail(username)
+				.orElseThrow(() -> new ResourceNotFoundException("User ", "Email: " + username, 0));
+
+		return user;
+	}
+
+}
